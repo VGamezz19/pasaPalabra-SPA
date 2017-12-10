5516a03b04d6935a5cefc6dfe31d5490302d1b16
VGamezz19 <victor.gamez1596@gmail.com>
2017-12-10 11:36:00 +0100

    back-ground

--- public/js/pasa-palabra.js
+++ public/js/pasa-palabra.js
@@ -1,31 +1,4 @@
 var questions =[]
-//  [
-//     { letter: "a", answer: "abducir", status: 0, question: "CON LA A. Dicho de una supuesta criatura extraterrestre: Apoderarse de alguien" },
-//     { letter: "b", answer: "bingo", status: 0, question: "CON LA B. Juego que ha sacado de quicio a todos los 'Skylabers' en las sesiones de precurso" },
-//     { letter: "c", answer: "churumbel", status: 0, question: "CON LA C. Niño, crío, bebé" },
-//     { letter: "d", answer: "diarrea", status: 0, question: "CON LA D. Anormalidad en la función del aparato digestivo caracterizada por frecuentes evacuaciones y su consistencia líquida" },
-//     { letter: "e", answer: "ectoplasma", status: 0, question: "CON LA E. Gelatinoso y se encuentra debajo de la membrana plasmática. Los cazafantasmas medían su radiación" },
-//     { letter: "f", answer: "facil", status: 0, question: "CON LA F. Que no requiere gran esfuerzo, capacidad o dificultad" },
-//     { letter: "g", answer: "galaxia", status: 0, question: "CON LA G. Conjunto enorme de estrellas, polvo interestelar, gases y partículas" },
-//     { letter: "h", answer: "harakiri", status: 0, question: "CON LA H. Suicidio ritual japonés por desentrañamiento" },
-//     { letter: "i", answer: "iglesia", status: 0, question: "CON LA I. Templo cristiano" },
-//     { letter: "j", answer: "jabali", status: 0, question: "CON LA J. Variedad salvaje del cerdo que sale en la película 'El Rey León', de nombre Pumba" },
-//     { letter: "l", answer: "licantropo", status: 0, question: "CON LA L. Hombre lobo" },
-//     { letter: "m", answer: "misantropo", status: 0, question: "CON LA M. Persona que huye del trato con otras personas o siente gran aversión hacia ellas" },
-//     { letter: "n", answer: "necedad", status: 0, question: "CON LA N. Demostración de poca inteligencia" },
-//     { letter: "ñ", answer: "señal", status: 0, question: "CONTIENE LA Ñ. Indicio que permite deducir algo de lo que no se tiene un conocimiento directo." },
-//     { letter: "o", answer: "orco", status: 0, question: "CON LA O. Humanoide fantástico de apariencia terrible y bestial, piel de color verde creada por el escritor Tolkien" },
-//     { letter: "p", answer: "protoss", status: 0, question: "CON LA P. Raza ancestral tecnológicamente avanzada que se caracteriza por sus grandes poderes psíonicos del videojuego StarCraft" },
-//     { letter: "q", answer: "queso", status: 0, question: "CON LA Q. Producto obtenido por la maduración de la cuajada de la leche" },
-//     { letter: "r", answer: "raton", status: 0, question: "CON LA R. Roedor" },
-//     { letter: "s", answer: "stackoverflow", status: 0, question:"CON LA S. Comunidad salvadora de todo desarrollador informático" },
-//     { letter: "t", answer: "terminator", status: 0, question: "CON LA T. Película del director James Cameron que consolidó a Arnold Schwarzenegger como actor en 1984" },
-//     { letter: "u", answer: "unamuno", status: 0, question: "CON LA U. Escritor y filósofo español de la generación del 98 autor del libro 'Niebla' en 1914" },
-//     { letter: "v", answer: "vikingos", status: 0, question: "CON LA V. Nombre dado a los miembros de los pueblos nórdicos originarios de Escandinavia, famosos por sus incursiones y pillajes en Europa" },
-//     { letter: "x", answer: "botox", status: 0, question: "CONTIENE LA X. Toxina bacteriana utilizada en cirujía estética" },
-//     { letter: "y", answer: "peyote", status: 0, question: "CONTIENE LA Y. Pequeño cáctus conocido por sus alcaloides psicoactivos utilizado de forma ritual y medicinal por indígenas americanos" },
-//     { letter: "z", answer: "zen", status: 0, question: "CON LA Z. Escuela de budismo que busca la experiencia de la sabiduría más allá del discurso racional" }
-// ]
 var user = '';
 var password = ''
 var time = 150;
@@ -161,11 +134,13 @@ function repetirPasaPalabra(element){
     element === true ? $('#modal-terminarPartida').modal('close') : console.log();
     document.getElementById('loaderHome').style.display = 'inline';
     document.getElementById('programa').style.display = 'none';
+    document.getElementById('footer').style.display = 'none'
+    document.getElementById('body').classList.remove('background');
     loaderHome();
     document.getElementById("timer").innerHTML = 153
     time = 153;
     statusTime = false
-    startTime(); 
+    startTime();  
     document.getElementById("puntos").innerHTML = 25;
     position = 0;
     
@@ -181,6 +156,8 @@ function loaderHome(){
     setTimeout(()=>{
         document.getElementById('loaderHome').style.display = 'none';
         document.getElementById('programa').style.display = 'inline';
+        document.getElementById('body').className = 'background'
+        document.getElementById('footer').style.display = 'inline'
     },3000)
 }
 function addCirculo(){
