54d6c1fab75241e2a9a3de32b9608c105e29214e
VGamezz19 <victor.gamez1596@gmail.com>
2017-12-10 22:33:01 +0100

    mongoose version

--- index.js
+++ index.js
@@ -14,29 +14,31 @@ app.use(methodOverride());
 
 
 //================MONGODB==================//
-var URI = process.env.MONGODB_URI || 'localhost/userPasaPalabra'
+var URI = 'mongodb://vgamez21:gordo3gordo2@ds135876.mlab.com:35876/heroku_4hnpjqbq?authSource=dbWithUserCredentials'//process.env.MONGODB_URI || 'localhost/userPasaPalabra'
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 var PORT = process.env.PORT || 5000
 var path = require('path')
 var engines = require('consolidate')
-
 var mongoose = require('mongoose');    
+console.log(URI)
 mongoose.Promise = global.Promise
-mongoose.connect(URI);
+mongoose.connect(URI)
+//mongoose.connect(URI+'?authMechanism=MONGODB-CR')
+//mongoose.connect(URI);
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 
 require('./models/user')
 require('./models/preguntas')
 
-// Since this is an example, we'll clean up after ourselves.
-mongoose.connection.db.collection('preguntaspasapalabras').drop(function (err) {
-if(err) throw err;
-    // Only close the connection when your app is terminating
-    mongoose.connection.db.close(function (err) {
-        if(err) throw err;
-    });
-});
+// // Since this is an example, we'll clean up after ourselves.
+// mongoose.connection.db.collection('preguntaspasapalabras').drop(function (err) {
+// if(err) throw err;
+//     // Only close the connection when your app is terminating
+//     mongoose.connection.db.close(function (err) {
+//         if(err) throw err;
+//     });
+// });
 
 
 
